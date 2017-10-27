80c180bf853982ddce98982022e6a6b2a3851702
Carter Tsai <hamming1@gmail.com>
2017-10-27 14:51:15 +0800

    middleware add DI to test

--- src/Middleware/MiddlewareTest.cs
+++ src/Middleware/MiddlewareTest.cs
@@ -1,18 +1,30 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using webapi.Framework.DAL;
 
 namespace webapi.Middleware
 {
     public class MiddlewareTest
     {
         private readonly RequestDelegate _next;
-        public MiddlewareTest(RequestDelegate next)
+        private readonly ILogger<MiddlewareTest> _logger;
+        private readonly IOperationTest _iop;
+
+        public MiddlewareTest(RequestDelegate next, 
+                              ILogger<MiddlewareTest> logger,
+                              IOperationTest iop)
         {
             _next = next;
+
+            _logger = logger;
+            _iop = iop;
+            _logger.LogInformation("MiddlewareTest init");
         }
         public async Task Invoke(HttpContext httpContext)
         {
-            System.Console.WriteLine("MiddlewareTest");
+            _logger.LogInformation("MiddlewareTest");
+            _logger.LogInformation(_iop.Get());
             await _next(httpContext);
         }
     }
